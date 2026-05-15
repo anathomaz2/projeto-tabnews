f1ab111814e37c7118c8410652491de76484d939
Ana Karla Thomaz <140630544+anathomaz2@users.noreply.github.com>
2026-05-15 01:55:29 +0000

    add calculadora testes e altera a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-  return <h1> Que massa! meu site ta no ar! </h1>;
+  return <h1> Oii Emersonnn, meu site ta no ar!! (colocar seu nome, pq se chamar de "mo" capaz de vc achar que é pra geral ne) </h1>;
 }
 
 export default Home;
---
+++ tests/calculadora.test.js
@@ -0,0 +1,7 @@
+test("nome do teste", () => {
+  console.log("e assim?")
+});
+
+test("testando outra condição do meu sistema",() => {
+  console.log("outro teste")
+})
